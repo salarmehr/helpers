9bc86bbb1bd80d0eb74679d9a48a1a08777bb52b
mehdi <mr.tavakolpour@yahoo.com>
2017-12-13 15:05:32 +0330

    mehdi's commit

--- tests/Test.php
+++ tests/Test.php
@@ -9,16 +9,32 @@ class Test extends PHPUnit\Framework\TestCase
     $testData = [
       ''                  => '',
       'sample'            => 'Sample',
-      'Sample'            => 'Sample',
-      'sampleStr'         => 'sample Str',
-      'SampleStr'         => 'Sample Str',
-      'SomeIDWithNumb3rs' => 'Some ID With Numb3rs',
-      'SomeID4Test'       => 'Some ID 4Test',
-      'ABCPvtLtd'         => 'ABC PvtLtd',
+      'sampleStr'         => 'Sample Str',
+      'someIDWithNumb3rs' => 'Some ID With Numb3rs',
+      'some ID 4Test'     => 'Some ID 4 Test',
+      'ABC pvtLtd'         => 'ABC Pvt Ltd',
       'ABC'               => 'ABC',
     ];
     foreach ($testData as $input => $output) {
       $this->assertEquals($output, f\str::decamlize($input));
+
+      var_dump($input);
+      var_dump($output);
     }
+  }
+
+  function testToEastern(){
+    $testData = [
+      'مهدی1' => 'مهدی۱',
+      '1−علی' => '۱−علی',
+      '2−احسان' => '۲−احسان',
+      '3−مهدی' => '۳−مهدی',
+    ];
+  
+
+    foreach ($testData as $input => $output) {
+      $this->assertEquals($output, f\num::toEastern($input));
+    }
+
   }
 }
